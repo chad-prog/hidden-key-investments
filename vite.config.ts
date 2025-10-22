531fc6fafa41685342eb3ed019993901d4f4eeae
Chad <chad@hiddenkeyinvestments.com>
2025-10-21 21:55:47 -0700

    update package.json, vite.config.ts, and tsconfig.node.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: true
-  },
   build: {
     outDir: 'dist',
-    sourcemap: false
+    sourcemap: false,
+    minify: 'esbuild'
+  },
+  server: {
+    port: 3000,
+    open: true
+  },
+  define: {
+    'process.env': {}
   }
 })
