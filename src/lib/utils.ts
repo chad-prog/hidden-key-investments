05a56e635c05d7779684c80138879d5d7dfbd343
Chad <chad@hiddenkeyinvestments.com>
2025-10-21 22:30:06 -0700

    refactor: enhance documentation for cn utility function in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+/**
+ * Utility function for merging Tailwind CSS classes
+ * Combines clsx and tailwind-merge for optimal class handling
+ * @param inputs - Class values to merge
+ * @returns {string} Merged class string
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
